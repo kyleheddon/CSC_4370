8ad1adb92020b6135aa771dc0e074e3da5aaffa2
Kyle Heddon <ktheddon@gmail.com>
2014-09-24 00:18:26 -0400

    moved files into app dir. Added vagrant box

---
+++ app/assignment3.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Assignment 3 - Checker Board</title>
+    <link href="assets/stylesheets/main.css" rel="stylesheet" type="text/css" />
+    <link href="assets/stylesheets/assignment3.css" rel="stylesheet" type="text/css" />
+    <meta content="initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" name="viewport">
+  </head>
+  <body>
+    <div id="contents" class="page-wrap">
+      <table>
+        <?php for($i = 0; $i < 8; $i++){ ?>
+          <tr>
+            <?php for($j = 0; $j < 8; $j++){ ?>
+              <td class="<?php echo ($i + $j)%2 == 0 ? 'red' : 'black' ?>"></td>
+            <?php } ?>
+          </tr>
+        <?php } ?>
+      </table>
+    </div>
+  </body>
+</html>
--- sync.rb
+++ sync.rb
@@ -1,17 +1,3 @@
 #!/usr/bin/env ruby
 
-whitelisted_file_types = %w(html ico txt jpg png)
-
-def directory_contains_file_type?(ext)
-  `ls | grep .#{ext}` != ''
-end
-
-file_types_to_copy = []
-
-whitelisted_file_types.each do |ext|
-  file_types_to_copy << ext if directory_contains_file_type?(ext)
-end
-
-wildcard_files = file_types_to_copy.map{|t|'*.'+t}.join(' ')
-
-`scp -r assets/ #{wildcard_files} csc4370FA14_59@codd.cs.gsu.edu:~/public_html/`
+`cd app && scp -r ./ csc4370FA14_59@codd.cs.gsu.edu:~/public_html/`
